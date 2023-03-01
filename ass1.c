065e698d1f9d31892e6ba3c1185d863ac1f1b601
kimikokel <kellyleong2002@gmail.com>
2023-03-01 22:39:47 +1100

    stage 1

--- ass1.c
+++ ass1.c
@@ -1,45 +1,3 @@
-/* Program to generate term-biased snippets for paragraphs of text.
-
-   Skeleton program written by Alistair Moffat, ammoffat@unimelb.edu.au,
-   August 2022, with the intention that it be modified by students
-   to add functionality, as required by the assignment specification.
-
-   Student Authorship Declaration:
-
-   (1) I certify that except for the code provided in the initial skeleton
-   file, the  program contained in this submission is completely my own
-   individual work, except where explicitly noted by further comments that
-   provide details otherwise.  I understand that work that has been developed
-   by another student, or by me in collaboration with other students, or by
-   non-students as a result of request, solicitation, or payment, may not be
-   submitted for assessment in this subject.  I understand that submitting for
-   assessment work developed by or in collaboration with other students or
-   non-students constitutes Academic Misconduct, and may be penalized by mark
-   deductions, or by other penalties determined via the University of
-   Melbourne Academic Honesty Policy, as described at
-   https://academicintegrity.unimelb.edu.au.
-
-   (2) I also certify that I have not provided a copy of this work in either
-   softcopy or hardcopy or any other form to any other student, and nor will I
-   do so until after the marks are released. I understand that providing my
-   work to other students, regardless of my intention or any undertakings made
-   to me by that other student, is also Academic Misconduct.
-
-   (3) I further understand that providing a copy of the assignment
-   specification to any form of code authoring or assignment tutoring service,
-   or drawing the attention of others to such services and code that may have
-   been made available via such a service, may be regarded as Student General
-   Misconduct (interfering with the teaching activities of the University
-   and/or inciting others to commit Academic Misconduct).  I understand that
-   an allegation of Student General Misconduct may arise regardless of whether
-   or not I personally make use of such solutions or sought benefit from such
-   actions.
-
-   Signed by: Kuan Pui Leong 1289672
-   Dated:     09/01/2023
-
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
@@ -65,25 +23,52 @@
 
 typedef char word_t[MAX_WORD_LEN+1];
 int get_word(word_t w, int limit);
+void print_para(word_t w);
+int get_para(word_t w, int* is_EOF);
 
 int main(int argc, char *argv[]) {
-	word_t w;
-	while (get_word(w, MAX_WORD_LEN) != EOF) {
-		printf("%s\n", w);
+	word_t w = "";
+	int is_EOF = 0;
+	int count = 0;
+	int length = 0;
+	int match = 0;
+	while(is_EOF == 0){
+		length = get_para(w, &is_EOF);
+		// printf("%d\n\n",length);
+		count++;
+		printf("======= Stage 1 [para %d; %d words]\n\n",count,length);
+		printf("======= Stage 2 [para %d; %d words; %d matches]\n\n",count,length,match);
 	}
+	
+	
+	// print_para(w);
 	
 	return 0;
 }
 
 int get_word(word_t W, int limit) {
 	int c;
-	/* first, skip over any non alphanumerics */
+	int count = 0;
 	while ((c=getchar())!=EOF && !isalnum(c)) {
+		if(c == '\n'){
+			count++;
+		}
 	}
+
+	if (count == 2) {
+		*W = c;
+		W += 1;
+		return PARA_END;
+	}
+
 	if (c==EOF) {
 		return EOF;
 	}
 	/* ok, first character of next word has been found */
+	if (strlen(W) > 0){
+		W += 1;
+	}
+
 	*W = c;
 	W += 1;
 	limit -= 1;
@@ -105,4 +90,31 @@ int get_word(word_t W, int limit) {
 	/* now close off the string */
 	*W = '\0';
 	return WORD_FND;
+}
+
+void print_para(word_t w){
+	while (get_word(w, MAX_WORD_LEN) != EOF) {
+		printf("%s ", w);
+	}
+}
+
+int get_para(word_t w, int* is_EOF){
+	int length = 0;
+	while (1) {
+		int result = get_word(w, MAX_WORD_LEN);
+		if(result == EOF){
+			*is_EOF = 1;
+			return length;
+		}
+		if(result == PARA_END){
+			return length;
+		}
+		if(result == WORD_FND){
+			// printf("%s ", w);
+			strcpy(w,"");
+			length++;
+		}
+		
+	} 
+	
 }
